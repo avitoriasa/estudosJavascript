86fb1f29ed8dfddc0d36ce7a4787a2410cf2bfbc
Vitória Sá <vitoriafs98@outlook.com>
2021-12-16 22:59:39 -0300

    ola

---
+++ AULA 3/script.js
@@ -0,0 +1,40 @@
+// NaN - Not a number
+// parceInt (inteiro)
+// parceFloat (com casas decimais)
+// Number (conversão para numero sem distinção)
+
+/*
+const num1 = 10;
+const num2 = parseFloat('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);
+
+*/
+
+// alert('Com a nossa mensagem');
+// confirm('deseja mesmo cancelar?');
+//prompt('digite o seu nome');
+// let confirma =  confirm ('uma mensagem');
+// console.log(confirma)
+
+/*
+const num1 = prompt('Digite um numero')
+const num2 = prompt('Digite outro numero')
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(`o resultado da sua conta foi: ${num1 + num2}`);
+*/
+ 
+/*
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+const varATemp = varA;
+varA = varB;
+varB = varC;
+varC = varATemp;
+
+console.log(varA, varB, varC); */
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # estudosJavascript
+
+Uma mescla do que vi e aprendi nos meus cursos :))
