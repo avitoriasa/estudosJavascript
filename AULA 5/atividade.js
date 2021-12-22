d95d55d68d6ba948a8cd47582cc4a831c8f3d8c1
Vitória Sá <vitoriafs98@outlook.com>
2021-12-21 22:42:49 -0300

    eaii

--- AULA 5/atividade.js
+++ AULA 5/atividade.js
@@ -1,4 +1,5 @@
 function comparaNumeros(num1, num2) {
+    if(!num1 || !num2) return 'Defina dois números!';
     const primeiraFrase = criaPrimeiraFrase (num1 , num2);
     const segundaFrase = criaSegundaFrase(num1, num2);
 
