2458a5e6ba31e66a4f9fc0ecf5b4f893e391569e
Vitória Sá <vitoriafs98@outlook.com>
2022-01-18 21:38:11 -0300

    eai

---
+++ AULA 6/script.js
@@ -0,0 +1,32 @@
+/*function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(2, 2));
+
+const raiz = function (n) {
+ return n ** 0.5;
+};
+
+console.log(raiz(9)) */
+
+/*const pessoa1 = {
+    nome: 'Vitoria',
+    sobrenome: 'Sá',
+    idade: '23'
+};
+
+console.log(pessoa1)*/
+
+/*function criaPessoa (nome, sobrenome, idade) {
+    return { nome, sobrenome, idade };
+}
+
+const pessoa1 = criaPessoa('Vitoria', 'Sá', 23);
+const pessoa2 = criaPessoa('Fernanda', 'Moreira', 44)
+const pessoa3 = criaPessoa('Maria', 'Socorro', 65)
+console.log(pessoa1) */
+
+// Valores primitivos
+
